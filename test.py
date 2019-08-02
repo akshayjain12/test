71ce06411f93b21e11e906478b9a78a8bba463bb
akshayjain12 <akshay.jain@tracxn.com>
2019-08-02 09:26:20 +0530

    first commit

---
+++ test.py
@@ -0,0 +1,8 @@
+import pandas as pd
+print('test completed')
+
+kulfi = ['choclate', 'vanila', 'butterscotch', 'kesar', 'mango']
+print(pd.Series(kulfi))
+
+lottery = [5,6,8,4,8,2,4,5,7,6,5]
+print(pd.Series(lottery))
